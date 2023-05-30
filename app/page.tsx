4512a05d0c22168eafabc66609b1fa4d702cd10b
Jonathan Word <91210536+jonword@users.noreply.github.com>
2023-05-30 16:33:16 -0500

    adjust page padding

--- app/page.tsx
+++ app/page.tsx
@@ -16,8 +16,8 @@ export default async function Home() {
   const data: Post[] = await getPosts();
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex flex-col my-16 min-w-[375px] max-w-[700px] w-full p-8">
+    <main className="flex min-h-screen flex-col items-center justify-between py-4 px-24">
+      <div className="flex flex-col min-w-[375px] max-w-[700px] w-full p-8">
         {data.map((post) => {
           return (
             <PostCard
