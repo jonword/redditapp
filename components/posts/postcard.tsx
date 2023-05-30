2bbba2c65f87e3173479410aafbf355d404ad91e
Jonathan Word <91210536+jonword@users.noreply.github.com>
2023-05-30 12:09:08 -0500

    remove destructured props

--- components/posts/postcard.tsx
+++ components/posts/postcard.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { FaArrowUp, FaArrowDown, FaRegCommentAlt } from "react-icons/fa";
 import shortenNumber from "@/util/shortenNumber";
 
-interface Props {
+export type Post = {
   id: number;
   icon_img: string;
   display_name: string;
@@ -16,50 +16,37 @@ interface Props {
   url: string;
   num_comments: number;
   ups: number;
-}
+};
 
-const PostCard = ({
-  id,
-  icon_img,
-  display_name,
-  subreddit_name_prefixed,
-  author,
-  title,
-  thumbnail,
-  post_hint,
-  reddit_video,
-  url,
-  num_comments,
-  ups,
-}: Props) => {
+const PostCard = (props: Post) => {
   return (
-    <div className="post-card" key={id}>
+    <div className="post-card" key={props.id}>
       <div className="post-details">
         <Image
           src={
-            icon_img ||
+            props.icon_img ||
             `https://www.redditinc.com/assets/images/site/reddit-logo.png`
           }
-          alt={display_name}
+          alt={props.display_name}
           className="post-icon"
         />
-        <p className="subreddit-name">{subreddit_name_prefixed}</p>
+        <p className="subreddit-name">{props.subreddit_name_prefixed}</p>
         <span>•</span>
-        <p className="post-author">Posted by u/{author}</p>
+        <p className="post-author">Posted by u/{props.author}</p>
       </div>
       <div className="title">
-        <p>{title}</p>
-        {thumbnail && post_hint === "image" ? (
+        <p>{props.title}</p>
+        {props.thumbnail && props.post_hint === "image" ? (
           <div className="media-container">
-            <Image src={url} className="post-media" alt="post" />
+            <Image src={props.url} className="post-media" alt="post" />
           </div>
         ) : (
           ""
         )}
-        {post_hint === "hosted:video" ? (
+        {props.post_hint === "hosted:video" ? (
           <div className="media-container">
             <video controls className="post-video">
-              <source src={reddit_video} type="video/mp4" />
+              <source src={props.reddit_video} type="video/mp4" />
             </video>
           </div>
         ) : (
@@ -69,11 +56,11 @@ const PostCard = ({
       <div className="comment-container">
         <div className="comment-icon">
           <FaRegCommentAlt />
-          <p>{num_comments}</p>
+          <p>{props.num_comments}</p>
         </div>
         <div className="post-votes">
           <FaArrowUp />
-          {ups}
+          {props.ups}
           <FaArrowDown />
         </div>
       </div>
