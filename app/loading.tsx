cbd52e5778317d279e9ca4e7a29cb79d705b62ae
Jonathan Word <91210536+jonword@users.noreply.github.com>
2023-06-07 15:27:34 -0500

    style loading screen

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,3 +1,9 @@
 export default function Loading() {
-  return <div>Loading...</div>;
+  return (
+    <div className="min-h-screen flex justify-center">
+      <div className="text-5xl p-24 font-bold animate-pulse text-gray-700">
+        Loading...
+      </div>
+    </div>
+  );
 }
