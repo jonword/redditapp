7290013487557fa89d644e5f8bb7eccc1f896f2f
Jonathan Word <91210536+jonword@users.noreply.github.com>
2023-06-07 15:33:07 -0500

    change loading text color

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,7 +1,7 @@
 export default function Loading() {
   return (
     <div className="min-h-screen flex justify-center">
-      <div className="text-5xl p-24 font-bold animate-pulse text-gray-700">
+      <div className="text-5xl p-24 font-bold animate-pulse text-gray-400">
         Loading...
       </div>
     </div>
